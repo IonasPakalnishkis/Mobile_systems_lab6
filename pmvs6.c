32878b8b1acf8e9ae59637af98f67b877b020d27
Ionas Pakalnishkis <bolivar1997@yandex.ru>
2016-10-24 17:13:41 +0300

    linux kernel coding style added

--- pmvs6.c
+++ pmvs6.c
@@ -13,77 +13,77 @@ static int timecount;
 static struct timer_list timer;
 
 static ssize_t timecount_show(struct kobject *kobj, struct kobj_attribute *attr,
-            char *buf)
+           char *buf)
 {
-    return sprintf(buf, "%d\n", timecount);
+	return sprintf(buf, "%d\n", timecount);
 }
 
 static void print_hello(unsigned long arg)
 {
-    printk("Hello, world! %d\n", timecount);
-    mod_timer(&timer, jiffies + timeout * HZ);
-    timecount--;
-    if(timecount == 0) {
-        del_timer(&timer);
-    }
+	printk("Hello, world! %d\n", timecount);
+   	mod_timer(&timer, jiffies + timeout * HZ);
+    	timecount--;
+    	if(timecount == 0) {
+        	del_timer(&timer);
+   	}
 }
 
 static ssize_t timecount_store(struct kobject *kobj, struct kobj_attribute *attr,
              const char *buf, size_t count)
 {
-    sscanf(buf, "%du", &timecount);
+    	sscanf(buf, "%du", &timecount);
 
-    if (timecount > 0) {
-        timer.expires = jiffies + timeout * HZ;
-        timer.function = print_hello;
-        add_timer(&timer);
-    } else {
-        del_timer_sync(&timer);
-    }
+    	if (timecount > 0) {
+        	timer.expires = jiffies + timeout * HZ;
+        	timer.function = print_hello;
+        	add_timer(&timer);
+    	} else {
+        	del_timer_sync(&timer);
+    	}
 
-    return count;
+    	return count;
 }
 
 static struct kobj_attribute timecount_attribute =
-    __ATTR(tion, 0664, timecount_show, timecount_store);
+    	__ATTR(tion, 0664, timecount_show, timecount_store);
 
 static struct attribute *attrs[] = {
-    &timecount_attribute.attr,
-    NULL,
+    	&timecount_attribute.attr,
+    	NULL,
 };
 
 static struct attribute_group attr_group = {
-    .attrs = attrs,
+    	.attrs = attrs,
 };
 
 static struct kobject *pmvs6_kobj;
 
 static int __init pmvs6_init(void)
 {
-    int retval;
+    	int retval;
 
-    pmvs6_kobj = kobject_create_and_add("mytimer", kernel_kobj);
-    if (!pmvs6_kobj)
-        return -ENOMEM;
+    	pmvs6_kobj = kobject_create_and_add("mytimer", kernel_kobj);
+    	if (!pmvs6_kobj)
+        	return -ENOMEM;
 
-    retval = sysfs_create_group(pmvs6_kobj, &attr_group);
-    if (retval)
-        goto error_group;
+    	retval = sysfs_create_group(pmvs6_kobj, &attr_group);
+    	if (retval)
+        	goto error_group;
 
-    init_timer(&timer);
+    	init_timer(&timer);
 
-    goto ok;
+    	goto ok;
 
 error_group:
-    kobject_put(pmvs6_kobj);
+    	kobject_put(pmvs6_kobj);
 
 ok:
-    return retval;
+    	return retval;
 }
 
 static void __exit pmvs6_exit(void)
 {
-    kobject_put(pmvs6_kobj);
+    	kobject_put(pmvs6_kobj);
 }
 
 module_init(pmvs6_init);
